de430b0bf1ae7152c79ebb4abe187f85d5fcc692
DPremReddy07 <2300031704@kluniversity.in>
2025-09-05 00:40:12 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './style.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to My React Project</h1>
+      <h1>CI/CD and Cloud DevOps Course</h1>
+      <h1>Done with GitPages</h1>
+    </div>
+  );
+}
+
+export default App;
